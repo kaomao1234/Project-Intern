dd6369194b80499b49d8497b26bc785c29b7eac9
Beginner-Project <boripats63@nu.ac.th>
2023-05-17 22:54:07 +0700

    Edit method

--- workshop01-app/src/viewmodel/homeviewmodel.js
+++ workshop01-app/src/viewmodel/homeviewmodel.js
@@ -1,19 +1,81 @@
-import { quantityModel, menuModel } from "../model";
-import { generateUID } from "../utils";
+import { OrderItemModel, OrderModel, MenuItemModel } from "../model";
+import { generateUID, OrderStatus } from "../utils";
 import { DataBase } from "../database";
 class HomeViewModel {
   constructor() {
     this.db = new DataBase();
-    this.menu = this.db.menu;
+    this.menu = this.db.readAllMenu();
+    this.currentOrderId = null;
+    this.options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
   }
-  increase(index) {}
-  decrease(index) {}
-  remove(index) {}
-  totalize() {
+
+  totalPrice() {
+    var value = 0;
+    this.readOrderItems().map((item, index) => {
+      value +=
+        this.menu.find((menuItem) => menuItem.id == item.menuItemId) *
+        item.quantity;
+    });
     return value;
   }
-  addTocart(muid) {}
 
-  removeFromcart(muid) {}
+  getMenuItem(id) {
+    return this.menu.find((menuItem) => menuItem.id == id);
+  }
+
+  createOrder(tableNumber) {
+    let orderId = generateUID();
+    this.currentOrderId = orderId;
+    
+    const date = Date.now();
+    let formattedDate = Intl.DateTimeFormat("en-US", this.options).format(date);
+    this.db.createOrder(
+      new OrderModel(
+        orderId,
+        tableNumber,
+        formattedDate,
+        OrderStatus.PENDING
+      ).toMap()
+    );
+  }
+
+  createOrderItem(menuItemId) {
+    const model = new OrderItemModel(
+      generateUID(),
+      this.currentorderId,
+      menuItemId,
+      1
+    );
+    console.log(model);
+    let isExistModel = false;
+    this.readOrderItems().map((item, index) => {
+      if (item.menuItemId == menuItemId) {
+        isExistModel = true;
+      }
+    });
+    if (!isExistModel) {
+      this.db.createOrderItem(model.toMap());
+    }
+  }
+  readOrderItems() {
+    let lst = [];
+    this.db.readOrderItems().forEach((item, index) => {
+      lst.push(OrderItemModel.fromMap(item));
+    });
+    return lst;
+  }
+
+  updateOrderItem(orderItemModel) {
+    this.db.updateOrderItem(orderItemModel.toMap());
+  }
+
+  deleteOrderItem(id) {
+    this.db.deleteOrderItem(id);
+  }
 }
 export default HomeViewModel;
