32dcf73eed86a79e5b81fa0bc755b19a00338948
Beginner-Project <boripats63@nu.ac.th>
2023-05-20 00:16:36 +0700

    Update index.js

--- workshop01-app/src/pages/index.js
+++ workshop01-app/src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./home";
+import MenuDetailsPage from "./menudetial";
+import ManageOrder from "./manageorder";
 
 const App = () => {
   return (
     <div>
-      <Home></Home>
+      <Home/>
     </div>
   );
 };
