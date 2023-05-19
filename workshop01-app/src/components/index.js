357c74b0fe6404931e09ebb9ecb75eb7cdc9cf82
Beginner-Project <boripats63@nu.ac.th>
2023-05-20 00:06:52 +0700

    feat(component): change name

--- workshop01-app/src/components/index.js
+++ workshop01-app/src/components/index.js
@@ -3,5 +3,6 @@ export { default as TextFieldIcon } from "./ui/textfield/textfieldicon";
 export { default as LeadingImageButton } from "./ui/button/leadingimage";
 export { default as MenuCard } from "./ui/card/menu";
 export { default as OrderItemField } from "./ui/textfield/orderitem";
-export { default as MenulistGrid } from "./ui/grid/menulist";
-export { default as OrderlistGrid} from "./ui/grid/orderlist"
+export { default as MenuListGrid } from "./ui/grid/menulist";
+export { default as OrderListGrid } from "./ui/grid/orderlist";
+export { default as TableSelectorModal } from "./ui/modal/tableselector";
