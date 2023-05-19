5867d42562522ed716d1b399faa46aea3c9191da
Beginner-Project <boripats63@nu.ac.th>
2023-05-20 00:19:38 +0700

    feat(feature): remove reducer
    cause: This feature will be set to act as Session, so delete it.
    next: I haven't figured it out yet
    
    Fixes #1

--- workshop01-app/src/store/feature/homecontroller/index.js
+++ workshop01-app/src/store/feature/homecontroller/index.js
@@ -1,47 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { HomeViewModel } from "../../../viewmodel";
-var viewmodel = new HomeViewModel();
 export const homeControllerSlice = createSlice({
   name: "homecontroller",
-  initialState: {
-    menu: viewmodel.menu,
-    totalPrice: 0,
-    orderItems: [],
-    table: [1, 2, 3, 4, 5],
-  },
-  reducers: {
-    createOrder: (state, action) => {
-      viewmodel.createOrder(action.payload);
-    },
-
-    createOrderItem: (state, action) => {
-      viewmodel.createOrderItem(action.payload);
-      state.orderItems = viewmodel.readOrderItems();
-    },
-    updateOrderItem: (state, action) => {
-      viewmodel.updateOrderItem(action.payload);
-      state.orderItems = viewmodel.readOrderItems();
-    },
-    deleteOrderItem: (state, action) => {
-      viewmodel.deleteOrderItem(action.payload.id);
-      state.orderItems = viewmodel.readOrderItems();
-    },
-    totalPrice: (state) => {
-      state.totalPrice = viewmodel.totalPrice();
-    },
-    deleteTableNumber: (state, action) => {
-      state.table.splice(action.payload);
-    },
-  },
+  initialState: {},
+  reducers: {},
 });
 
-export const {
-  createOrder,
-  createOrderItem,
-  updateOrderItem,
-  deleteOrderItem,
-  totalPrice,
-  deleteTableNumber,
-} = homeControllerSlice.actions;
+export const {} = homeControllerSlice.actions;
 
 export default homeControllerSlice.reducer;
