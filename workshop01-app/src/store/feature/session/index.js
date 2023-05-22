674d3e332fa60beb52d02cf4ca6bac15f17436e3
Beginner-Project <boripats63@nu.ac.th>
2023-05-22 22:44:40 +0700

    feat(feature->session): create new session

---
+++ workshop01-app/src/store/feature/session/index.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const sessionSlice = createSlice({
+  name: "session",
+  initialState: {
+    value:{}
+  },
+  reducers: {
+    setSession: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+export const {setSession} = sessionSlice.actions;
+export default sessionSlice.reducer;
