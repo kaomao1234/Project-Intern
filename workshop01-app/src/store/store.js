e04bf745ea5a2227259ebcdc854bc61467f6de3e
Beginner-Project <boripats63@nu.ac.th>
2023-05-22 22:44:45 +0700

    Update store.js

--- workshop01-app/src/store/store.js
+++ workshop01-app/src/store/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import homeControllerSlice from "./feature/homecontroller";
+import homeViewModelSlice from "./feature/homeviewmodel";
+import menuDetailControllerSlice from "./feature/menudetailcontroller";
+import sessionSlice from "./feature/session";
 export const store = configureStore({
   reducer: {
-    homecontroller: homeControllerSlice,
+    homeviewmodel: homeViewModelSlice,
+    menudetailcontroller: menuDetailControllerSlice,
+    session: sessionSlice,
   },
 });
