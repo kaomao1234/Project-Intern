e52880c473678c7a89018b45972e5e2a7522a73e
Beginner-Project <boripats63@nu.ac.th>
2023-05-23 22:49:44 +0700

    Delete index.js

--- workshop01-app/src/store/feature/menudetailviewmodel/index.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-export const menuDetailViewModelSlice = createSlice({
-  name: "menudetailviewmodel",
-  initialState: {},
-  reducers: {},
-});
-export const {} = menuDetailViewModelSlice.actions;
-export default menuDetailViewModelSlice.reducer;
