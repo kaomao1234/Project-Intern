e99dbe4c60fada972d6cb092e965f770de4bda00
Beginner-Project <boripats63@nu.ac.th>
2023-05-24 19:40:40 +0700

    feat(viewmodel-menudetailviewmodel): Update createOrder functionality
    Additional section:
     - If the selected menu is already selected, the quantity will be added to the existing menu.

--- workshop01-app/src/viewmodel/menudetailviewmodel.js
+++ workshop01-app/src/viewmodel/menudetailviewmodel.js
@@ -6,18 +6,36 @@ class MenuDetailViewModel {
   constructor() {
     this.db = new DataBase();
   }
-  createOrderItem(menuId, quantity, tableNumber) {
-    console.log(tableNumber);
-    this.db.read(`orders/${tableNumber}`).then((snapshot) => {
-      if (snapshot.exists()) {
-        const value = snapshot.val();
-        this.db.create(`orderItems/${generateUID()}`, {
-          menuId: menuId,
-          quantity: quantity,
-          orderId: value.id,
-        });
+
+  async createOrderItem(menuId, quantity, tableNumber) {
+    const order = await this.db.read(`orders/${tableNumber}`);
+    const orderItems = await this.db.read(`orderItems`);
+
+    if (orderItems.exists()) {
+      const orderItemsData = orderItems.val();
+      const existingOrderItem = {};
+      let exisitingOrderItemId = 0;
+      Object.entries(orderItemsData).forEach(([id, object]) => {
+        if (object.orderId == order.val().id && object.menuId == menuId) {
+          Object.assign(existingOrderItem, object);
+          exisitingOrderItemId = id;
+        }
+      });
+      if (exisitingOrderItemId != 0) {
+        existingOrderItem.quantity += quantity; // Increment the quantity by the provided quantity
+        this.db.update(`orderItems/${exisitingOrderItemId}`, existingOrderItem);
+        return; // Order item already exists, so no need to create a new one
       }
-    });
+    }
+
+    const newOrderItemId = generateUID();
+    const newOrderItemData = {
+      menuId: menuId,
+      quantity: quantity,
+      orderId: order.val().id,
+    };
+
+    this.db.create(`orderItems/${newOrderItemId}`, newOrderItemData);
   }
 }
 
