6bd2770c3323c87001cfbf66ff79fd1a6673312d
Beginner-Project <boripats63@nu.ac.th>
2023-06-06 22:39:32 +0700

    Update index.js

--- workshop01-app/src/viewmodel/index.js
+++ workshop01-app/src/viewmodel/index.js
@@ -1,3 +1,4 @@
 export { default as HomeViewModel } from "./homeviewmodel";
 export { default as MenuDetailViewModel } from "./menudetailviewmodel";
-export { default as ManageOrderViewModel } from "./manageorderviewmodel";
+export { default as AdminViewModel } from "./adminviewmodel";
+export { default as OrderDetailViewModel } from "./orderdetailviewmodel";
