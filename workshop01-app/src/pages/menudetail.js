c6a477e0168e988315afb89069e82f1a181e0364
Beginner-Project <boripats63@nu.ac.th>
2023-06-06 22:37:13 +0700

    feat(page->menudetail): Switched from using useState to redux instead.

--- workshop01-app/src/pages/menudetail.js
+++ workshop01-app/src/pages/menudetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Typography,
   Button,
@@ -10,35 +10,53 @@ import {
   InputAdornment,
   IconButton,
 } from "@mui/material";
-import { Add, Remove } from "@mui/icons-material";
+import { Add, Login, Remove } from "@mui/icons-material";
 import { useRouter, withRouter } from "next/router";
-import { useSelector } from "react-redux";
-import { MenuDetailViewModel } from "@/viewmodel";
-const viewmodel = new MenuDetailViewModel();
+import { useSelector, useDispatch } from "react-redux";
+import { MenuDetailViewModel } from "../viewmodel";
+import { setState } from "../store/feature/menudetail";
 const MenuDetailPage = () => {
-  const model = useSelector((state) => state.session.value.model);
-  const tableNumber = useSelector((state) => state.session.value.table);
-  const [quantity, setQuantity] = useState(1); // State for quantity
+  //   const [stateValue, setStateValue] = useState(null);
+  const dispatch = useDispatch();
+  const viewmodelRef = useRef(new MenuDetailViewModel());
+  const viewmodel = viewmodelRef.current;
   const router = useRouter();
-  const handleQuantityChange = (value) => {
-    setQuantity(value);
-  };
 
+  useEffect(() => {
+      if (Object.keys(router.query).length > 0) {
+      dispatch(
+        setState({
+          menu: JSON.parse(router.query.menu),
+          selectedTable: router.query.selectedTable,
+        })
+      );
+    }
+  }, [dispatch, router.query]);
+  const menu = useSelector((state) => state.menudetail.menu);
+  const quantity = useSelector((state) => state.menudetail.quantity);
+  const selectedTable = useSelector((state) => state.menudetail.selectedTable);
+  const available = useSelector((state)=>state.menudetail.available);
   const handleAddToCart = () => {
     // Add logic to handle adding to cart with the selected quantity
-    viewmodel.createOrderItem(model.id, quantity, tableNumber);
-    router.push("/home");
+    viewmodel.createOrderItem(menu.id, quantity, selectedTable);
+    router.push(`/home?selectedTable=${selectedTable}`);
   };
-
-  if (model == undefined) {
+  const handleQuantityChange = (value) => {
+    dispatch(
+      setState({
+        quantity: value,
+      })
+    );
+  };
+  if (!available) {
+    // Render a loading state or return null while waiting for data
     return (
       <Typography
-        variant="h1"
         sx={{
           color: "black",
         }}
       >
-        Back
+        Loading...
       </Typography>
     );
   }
@@ -47,7 +65,7 @@ const MenuDetailPage = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h4" component="h1">
-          {model.name}
+          {menu.name}
         </Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
@@ -55,16 +73,20 @@ const MenuDetailPage = () => {
           <CardMedia
             component="img"
             height="300"
-            image={model.imageSrc}
-            alt={model.name}
+            image={menu.imageSrc}
+            alt={menu.name}
           />
         </Card>
       </Grid>
       <Grid item xs={12} sm={6} md={8}>
         <Card>
           <CardContent>
-            <Typography variant="body1">{model.description}</Typography>
-            <Typography variant="h6">Price: {model.price}</Typography>
+            <Typography variant="body1">
+              {menu.description}
+            </Typography>
+            <Typography variant="h6">
+              Price: {menu.price}
+            </Typography>
             <Typography variant="subtitle1">Ingredients:</Typography>
             <Grid container spacing={2} alignItems="center" sx={{ m: "10px" }}>
               <Grid item>
@@ -85,12 +107,16 @@ const MenuDetailPage = () => {
                     endAdornment: (
                       <InputAdornment position="end">
                         <IconButton
-                          onClick={() => handleQuantityChange(quantity - 1)}
+                          onClick={() =>
+                            handleQuantityChange(quantity - 1)
+                          }
                         >
                           <Remove />
                         </IconButton>
                         <IconButton
-                          onClick={() => handleQuantityChange(quantity + 1)}
+                          onClick={() =>
+                            handleQuantityChange(quantity + 1)
+                          }
                         >
                           <Add />
                         </IconButton>
