16c8fde677bb98b14af52efe55fc72dcbf513df7
Beginner-Project <boripats63@nu.ac.th>
2023-06-12 22:29:56 +0700

    feat: update codekata
    borntodev task 222

---
+++ code-kata/task222.js
@@ -0,0 +1,5 @@
+function compile(arg){
+    let input = arg.split(" ").reverse().join(" ");
+    console.log(input);
+}
+compile("life. social crumbling a and responsibilities, bills, with except again old years six be to good feel it does DAMN AND Bandicoot Crash played first I when 6 was I");
