48d56a270ecb2ec64bfc51908d0523609810c03d
Beginner-Project <boripats63@nu.ac.th>
2023-06-13 21:49:03 +0700

    feat(valueListenableBuilder): add comment for suggestion

--- workshop02-app/src/hooks/valueListenableBuilder.tsx
+++ workshop02-app/src/hooks/valueListenableBuilder.tsx
@@ -10,12 +10,18 @@ type State = {
   value: any;
 };
 
+/**
+ * A React component that listens to changes in a `valueListenable` object and renders
+ * the result of the `builder` function with the current value.
+ *
+ * @param {ValueNotifier} valueListenable - The valueListenable object that the component listens to for changes.
+ * @param {Function} builder - A builder function that takes the current value and returns the ReactNode to render.
+ */
 class ValueListenableBuilder extends Component<Props, State> {
+
   listener: () => void;
 
   constructor(props: Props) {
-
-
     super(props);
 
     // Initialize the state with the initial value from valueListenable
@@ -30,12 +36,12 @@ class ValueListenableBuilder extends Component<Props, State> {
   }
 
   componentDidMount() {
-    // Add the listener to valueListenable when the component is mounted
+    /* Add the listener to valueListenable when the component is mounted */
     this.props.valueListenable.addListener(this.listener);
   }
 
   componentWillUnmount() {
-    // Remove the listener from valueListenable when the component is unmounted
+    /* Remove the listener from valueListenable when the component is unmounted */
     this.props.valueListenable.removeListener(this.listener);
   }
 
